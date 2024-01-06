36ec713ca7945866caf7d2d0ac256a92bd402ec8
RezzRoshandel <138697199+RezzRoshandel@users.noreply.github.com>
2024-01-06 18:19:30 +0400

    Update ryan.php

--- ryan.php
+++ ryan.php
@@ -6,7 +6,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
+  if($check !== true) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
