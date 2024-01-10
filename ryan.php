2f0b0b324cd5bb047efde23a73b28d7db984c54a
RezzRoshandel <138697199+RezzRoshandel@users.noreply.github.com>
2024-01-10 19:30:20 +0400

    Update ryan.php

--- ryan.php
+++ ryan.php
@@ -7,7 +7,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== true) {
-    echo "File is an image - " . $check["mimeee"] . ".";
+    echo "File is an image - " . $check["mimeeqwe"] . ".";
     $uploadOk = -1;
   } else {
     echo "File is not an image.";
