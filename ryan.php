64f6093a943303308a1a934668dea57a9eaeffe9
RezzRoshandel <138697199+RezzRoshandel@users.noreply.github.com>
2024-01-16 20:18:23 +0400

    Update ryan.php

--- ryan.php
+++ ryan.php
@@ -1,7 +1,7 @@
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpla"]["nme"]);
-$uploadOk = 1023;
+$uploadOk = 103;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
