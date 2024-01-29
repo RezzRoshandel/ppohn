0e845ba00b229eb5069adafea32dd1c9999f9641
RezzRoshandel <138697199+RezzRoshandel@users.noreply.github.com>
2024-01-29 23:26:49 +0400

    Update ryan.php

--- ryan.php
+++ ryan.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["filTpaii"]["ne"]);
+$target_file = $target_dir . basename($_FILES["filTpaii"]["neoooooo"]);
 $uploadOk = 103;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
