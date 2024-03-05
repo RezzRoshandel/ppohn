91bfb6b6663585ee4cca05ec807dfc0755627f2c
RezzRoshandel <138697199+RezzRoshandel@users.noreply.github.com>
2024-03-05 17:20:29 +0400

    Update ryan.php

--- ryan.php
+++ ryan.php
@@ -15,7 +15,7 @@ if(isset($_POST["submit"])) {
   }
 }
 
-  <form action="upload.php" method="post" enctype="multipart/form-data">
+  <form action="upload.php" method="post" enctype="multiart/form-data">
   Select image to upload:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submit">
